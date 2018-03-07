3269f88686db2f8a9a46411f9638d581be6fbfe3
shuanglengyunji <liuhan211211@gmail.com>
2018-03-07 11:09:51 +0800

    Clear file: modified:   dev/Utilities/STM32_EVAL/STM3210B_EVAL/stm3210b_eval.c modified:   dev/Utilities/STM32_EVAL/STM3210B_EVAL/stm3210b_eval.h

--- dev/Utilities/STM32_EVAL/STM3210B_EVAL/stm3210b_eval.h
+++ dev/Utilities/STM32_EVAL/STM3210B_EVAL/stm3210b_eval.h
@@ -121,6 +121,7 @@ typedef enum
   * @{
   */
 #define LEDn                             4
+
 #define LED1_PIN                         GPIO_Pin_6
 #define LED1_GPIO_PORT                   GPIOC
 #define LED1_GPIO_CLK                    RCC_APB2Periph_GPIOC  
@@ -263,93 +264,7 @@ typedef enum
 
 /**
   * @}
-  */ 
-
-/** @addtogroup STM3210B_EVAL_LOW_LEVEL_SD_SPI
-  * @{
   */
-/**
-  * @brief  SD SPI Interface pins
-  */
-#define SD_SPI                           SPI1
-#define SD_SPI_CLK                       RCC_APB2Periph_SPI1
-#define SD_SPI_SCK_PIN                   GPIO_Pin_5                  /* PA.05 */
-#define SD_SPI_SCK_GPIO_PORT             GPIOA                       /* GPIOA */
-#define SD_SPI_SCK_GPIO_CLK              RCC_APB2Periph_GPIOA
-#define SD_SPI_MISO_PIN                  GPIO_Pin_6                  /* PA.06 */
-#define SD_SPI_MISO_GPIO_PORT            GPIOA                       /* GPIOA */
-#define SD_SPI_MISO_GPIO_CLK             RCC_APB2Periph_GPIOA
-#define SD_SPI_MOSI_PIN                  GPIO_Pin_7                  /* PA.07 */
-#define SD_SPI_MOSI_GPIO_PORT            GPIOA                       /* GPIOA */
-#define SD_SPI_MOSI_GPIO_CLK             RCC_APB2Periph_GPIOA
-#define SD_CS_PIN                        GPIO_Pin_12                 /* PC.12 */
-#define SD_CS_GPIO_PORT                  GPIOC                       /* GPIOC */
-#define SD_CS_GPIO_CLK                   RCC_APB2Periph_GPIOC
-#define SD_DETECT_PIN                    GPIO_Pin_7                  /* PE.07 */
-#define SD_DETECT_GPIO_PORT              GPIOE                       /* GPIOE */
-#define SD_DETECT_GPIO_CLK               RCC_APB2Periph_GPIOE
-
-/**
-  * @}
-  */
-
-/** @addtogroup STM3210B_EVAL_LOW_LEVEL_M25P_FLASH_SPI
-  * @{
-  */
-/**
-  * @brief  M25P FLASH SPI Interface pins
-  */  
-#define sFLASH_SPI                       SPI1
-#define sFLASH_SPI_CLK                   RCC_APB2Periph_SPI1
-#define sFLASH_SPI_SCK_PIN               GPIO_Pin_5                  /* PA.05 */
-#define sFLASH_SPI_SCK_GPIO_PORT         GPIOA                       /* GPIOA */
-#define sFLASH_SPI_SCK_GPIO_CLK          RCC_APB2Periph_GPIOA
-#define sFLASH_SPI_MISO_PIN              GPIO_Pin_6                  /* PA.06 */
-#define sFLASH_SPI_MISO_GPIO_PORT        GPIOA                       /* GPIOA */
-#define sFLASH_SPI_MISO_GPIO_CLK         RCC_APB2Periph_GPIOA
-#define sFLASH_SPI_MOSI_PIN              GPIO_Pin_7                  /* PA.07 */
-#define sFLASH_SPI_MOSI_GPIO_PORT        GPIOA                       /* GPIOA */
-#define sFLASH_SPI_MOSI_GPIO_CLK         RCC_APB2Periph_GPIOA
-#define sFLASH_CS_PIN                    GPIO_Pin_4                  /* PA.04 */
-#define sFLASH_CS_GPIO_PORT              GPIOA                       /* GPIOA */
-#define sFLASH_CS_GPIO_CLK               RCC_APB2Periph_GPIOA 
-
-/**
-  * @}
-  */
-
-/** @addtogroup STM3210B_EVAL_LOW_LEVEL_TSENSOR_I2C
-  * @{
-  */
-/**
-  * @brief  LM75 Temperature Sensor I2C Interface pins
-  */  
-#define LM75_I2C                         I2C1
-#define LM75_I2C_CLK                     RCC_APB1Periph_I2C1
-#define LM75_I2C_SCL_PIN                 GPIO_Pin_6                  /* PB.06 */
-#define LM75_I2C_SCL_GPIO_PORT           GPIOB                       /* GPIOB */
-#define LM75_I2C_SCL_GPIO_CLK            RCC_APB2Periph_GPIOB
-#define LM75_I2C_SDA_PIN                 GPIO_Pin_7                  /* PB.07 */
-#define LM75_I2C_SDA_GPIO_PORT           GPIOB                       /* GPIOB */
-#define LM75_I2C_SDA_GPIO_CLK            RCC_APB2Periph_GPIOB
-#define LM75_I2C_SMBUSALERT_PIN          GPIO_Pin_5                  /* PB.05 */
-#define LM75_I2C_SMBUSALERT_GPIO_PORT    GPIOB                       /* GPIOB */
-#define LM75_I2C_SMBUSALERT_GPIO_CLK     RCC_APB2Periph_GPIOB
-#define LM75_I2C_DR                      ((uint32_t)0x40005410)
-
-#define LM75_DMA_CLK                     RCC_AHBPeriph_DMA1
-#define LM75_DMA_TX_CHANNEL              DMA1_Channel6
-#define LM75_DMA_RX_CHANNEL              DMA1_Channel7
-#define LM75_DMA_TX_TCFLAG               DMA1_FLAG_TC6
-#define LM75_DMA_RX_TCFLAG               DMA1_FLAG_TC7 
-
-/**
-  * @}
-  */
-    
-/**
-  * @}
-  */ 
   
 /** @defgroup STM3210B_EVAL_LOW_LEVEL_Exported_Macros
   * @{
@@ -365,15 +280,11 @@ void STM_EVAL_LEDInit(Led_TypeDef Led);
 void STM_EVAL_LEDOn(Led_TypeDef Led);
 void STM_EVAL_LEDOff(Led_TypeDef Led);
 void STM_EVAL_LEDToggle(Led_TypeDef Led);
+
 void STM_EVAL_PBInit(Button_TypeDef Button, ButtonMode_TypeDef Button_Mode);
 uint32_t STM_EVAL_PBGetState(Button_TypeDef Button);
+
 void STM_EVAL_COMInit(COM_TypeDef COM, USART_InitTypeDef* USART_InitStruct);
-void SD_LowLevel_DeInit(void);
-void SD_LowLevel_Init(void); 
-void sFLASH_LowLevel_DeInit(void);
-void sFLASH_LowLevel_Init(void); 
-void LM75_LowLevel_DeInit(void);
-void LM75_LowLevel_Init(void); 
  
 /**
   * @}
