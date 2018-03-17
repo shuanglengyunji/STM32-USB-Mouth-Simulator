68d0bd468c3a1379baad3d1b1fe49161eca808d5
shuanglengyunji <liuhan211211@gmail.com>
2018-03-17 12:18:04 +0800

    rise the band of com2 to 115200 during inti.

--- dev/Libraries/CH375/interface.h
+++ dev/Libraries/CH375/interface.h
@@ -12,12 +12,13 @@
 
 #define ERR_USB_UNKNOWN  0xFA
 
-extern void CH375_PORT_INIT( void );  		/* CH375通讯接口初始化 */
-extern void xWriteCH375Cmd( uint8_t mCmd );	/* 向CH375写命令 */
-extern void xWriteCH375Data( uint8_t mData );	/* 向CH375写数据 */
-extern uint8_t xReadCH375Data( void );			/* 从CH375读数据 */
-extern uint8_t Query375Interrupt( void );		/* 查询CH375中断(INT#引脚为低电平) */
-extern uint8_t mInitCH375Host( void );			/* 初始化CH375 */
+void CH375_PORT_INIT( void );  		/* CH375通讯接口初始化 */
+void CH375_COM_INIT( u32 band );  		/* 串口通信波特率 */
+void xWriteCH375Cmd( uint8_t mCmd );	/* 向CH375写命令 */
+void xWriteCH375Data( uint8_t mData );	/* 向CH375写数据 */
+uint8_t xReadCH375Data( void );			/* 从CH375读数据 */
+uint8_t Query375Interrupt( void );		/* 查询CH375中断(INT#引脚为低电平) */
+uint8_t mInitCH375Host( void );			/* 初始化CH375 */
 
 #endif
 
--- dev/Libraries/CH375/uart.c
+++ dev/Libraries/CH375/uart.c
@@ -19,6 +19,42 @@ void CH375_PORT_PRE_INIT(void)
 	GPIO_SetBits(GPIOA, GPIO_Pin_3);	//复位期间，CH375的Tx口应该为高，这样才能进入串口模式
 }
 
+void CH375_COM_INIT( u32 band )
+{	
+	// USART2
+	GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+	
+	//首先关掉串口
+	USART_Cmd(USART2, DISABLE);
+	
+	/* config USART2 clock */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+	
+	/* USART1 GPIO config */
+	/* Configure USART2 Tx (PA.02) as alternate function push-pull */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+	/* Configure USART2 Rx (PA.03 as input floating */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+	/* USART1 mode config */
+	USART_InitStructure.USART_BaudRate = band;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No ;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART2, &USART_InitStructure);
+	
+	USART_Cmd(USART2, ENABLE);
+}
+
 //接口硬件初始化
 void CH375_PORT_INIT( void )
 {
@@ -36,36 +72,8 @@ void CH375_PORT_INIT( void )
 	
 	///////////////////////////////////////////////////////////////////
 	
-	// USART2
+	CH375_COM_INIT(9600);	//上电初始化波特率为9600
 	
-	USART_InitTypeDef USART_InitStructure;
-	
-	/* config USART1 clock */
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
-	
-	/* USART1 GPIO config */
-	/* Configure USART2 Tx (PA.02) as alternate function push-pull */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	
-	/* Configure USART2 Rx (PA.03 as input floating */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	
-	/* USART1 mode config */
-	USART_InitStructure.USART_BaudRate = 9600;
-	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-	USART_InitStructure.USART_StopBits = USART_StopBits_1;
-	USART_InitStructure.USART_Parity = USART_Parity_No ;
-	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-	USART_Init(USART2, &USART_InitStructure);
-	
-	USART_Cmd(USART2, ENABLE);
-
 }
 
 //写命令
@@ -132,7 +140,28 @@ uint8_t	mInitCH375Host( void )  /* 初始化CH375 */
 	res = xReadCH375Data( );
 	if ( res != 0x9A ) 
 		return( ERR_USB_UNKNOWN );  /* 通讯接口不正常,可能原因有:接口连接异常,其它设备影响(片选不唯一),串口波特率,一直在复位,晶振不工作 */
-
+	
+	/////////////////////////////////////////////////////////////////////////////////////
+	
+	/* 将串口波特率提高 */
+	xWriteCH375Cmd( CMD_SET_BAUDRATE );  //设置USB波特率
+	xWriteCH375Data( 0x03 );	//0x03 0xCC -- 115200     0x03 0xF3 -- 460800
+	xWriteCH375Data( 0xCC );
+	CH375_COM_INIT(115200);		//切换本地串口波特率
+	delay_ms(1);	//延时1ms
+	res = xReadCH375Data( );
+	if ( res != CMD_RET_SUCCESS ) 
+		return( ERR_USB_UNKNOWN );
+	
+	/* 重新测试单片机与CH375之间的通讯接口 */
+	xWriteCH375Cmd( CMD_CHECK_EXIST );  
+	xWriteCH375Data( 0x65 );
+	res = xReadCH375Data( );
+	if ( res != 0x9A ) 
+		return( ERR_USB_UNKNOWN );  /* 通讯接口不正常,可能原因有:接口连接异常,其它设备影响(片选不唯一),串口波特率,一直在复位,晶振不工作 */
+	
+	/////////////////////////////////////////////////////////////////////////////////////
+	
 	/* 设备USB工作模式 */
 	xWriteCH375Cmd( CMD_SET_USB_MODE );  
 	xWriteCH375Data( 0x06 );
